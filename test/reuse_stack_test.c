f3a3425d127e12e64dfdacc579a50b10f8da3b5f
David Chu <thedavid.chu@mail.utoronto.ca>
2023-12-08 21:44:10 -0500

    Add (non-compiling) reuse stack

---
+++ test/reuse_stack_test.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdbool.h>
+
+#include "reuse_stack/reuse_stack.h"
+
+bool
+access_same_key_five_times()
+{
+    struct ReuseStack *reuse_stack = reuse_stack_new();
+
+    reuse_stack_access_item(reuse_stack, 0);
+    reuse_stack_access_item(reuse_stack, 0);
+    reuse_stack_access_item(reuse_stack, 0);
+    reuse_stack_access_item(reuse_stack, 0);
+    reuse_stack_access_item(reuse_stack, 0);
+
+    if (me->histogram[0] != 4 || me->infinite_distance != 1) {
+        assert(0 && "histogram should be {0: 4, inf: 1}");
+        reuse_stack_free(reuse_stack);
+        return false;
+    }
+
+    reuse_stack_free(reuse_stack);
+}
+
+int
+main(void)
+{
+    bool r = false;
+    r = access_same_key_five_times();
+    if (!r) {
+        assert(0 && "should have passed lol");
+    }
+    return 0;
+}
