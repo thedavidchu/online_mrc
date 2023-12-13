8b1b080208f249a4a1540172be01bb8aa9a31721
David Chu <thedavid.chu@mail.utoronto.ca>
2023-12-13 15:53:09 -0500

    Format with newline at EOF

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
 #include <glib.h>
+#include <stdio.h>
 
-int main(void) {
+int
+main(void)
+{
     printf("Hello, World!\n");
-    #ifdef G_OS_UNIX
-        printf("UNIX!\n");
-    #endif
+#ifdef G_OS_UNIX
+    printf("UNIX!\n");
+#endif
     return 0;
 }
