83ce18b89420f31760e7ad4ddb63b99cdc9fe0e7
David Chu <thedavid.chu@mail.utoronto.ca>
2023-12-15 17:46:18 -0500

    Add plotting script for MRC

---
+++ script/plot_mrc.py
@@ -0,0 +1,37 @@
+import json
+from typing import Dict, Union
+import matplotlib.pyplot as plt
+
+
+def input_histogram() -> Dict[float, Union[int, float]]:
+    while True:
+        histogram = input()
+        # NOTE  We assume that there will only be a single JSON. This will be
+        #       only line that starts with the character '{' and the entire
+        #       JSON string will be on a single line.
+        if histogram.startswith(r"{"):
+            break
+    histogram = json.loads(histogram)
+    histogram = {float(key): value for key, value in histogram.items()}
+    return histogram
+
+def plot_histogram(histogram: Dict[float, Union[float, int]]):
+    running_total = 0
+    total = sum(histogram.values())
+    mrc = {}
+    for key, value in histogram.items():
+        running_total += value
+        mrc[key] = 1 - running_total / total
+    plt.figure()
+    plt.title("Miss-Rate Curve")
+    plt.xlabel("Number of key-value pairs")
+    plt.ylabel("Miss-rate")
+    plt.plot(mrc.keys(), mrc.values())
+    plt.savefig("mrc.png")
+
+def main():
+    histogram = input_histogram()
+    plot_histogram(histogram)
+
+if __name__ == "__main__":
+    main()
