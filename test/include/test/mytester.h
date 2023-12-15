39385719ed56aafeafedfeff6dd5aa07893c9f83
David Chu <thedavid.chu@mail.utoronto.ca>
2023-12-15 12:36:30 -0500

    Add additional macro to tester

--- test/include/test/mytester.h
+++ test/include/test/mytester.h
@@ -13,3 +13,17 @@
         }                                                                                          \
         printf("[SUCCESS] %s:%d " #func_call " succeeded!\n", __FILE__, __LINE__);                 \
     } while (0)
+
+#define ASSERT_TRUE_OR_CLEANUP(func_call, cleanup)                                                 \
+    do {                                                                                           \
+        bool r = func_call;                                                                        \
+        if (!r) {                                                                                  \
+            /* Clean up resources */                                                               \
+            cleanup;                                                                               \
+            printf("[ERROR] %s:%d\n", __FILE__, __LINE__);                                         \
+            /* NOTE This assertion is for debugging purposes so that we have a                     \
+            finer grain understanding of where the failure occurred. */                            \
+            assert(0 && "exit on failure!");                                                       \
+            return false;                                                                          \
+        }                                                                                          \
+    } while (0)
