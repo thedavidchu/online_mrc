45a6b367eec0c527d74c44946117241db76eed03
David Chu <thedavidchu@gmail.com>
2024-04-30 20:31:41 -0400

    Add single threaded hash table

---
+++ src/lib/lookup/include/lookup/hash_table.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "lookup/lookup.h"
+#include "types/entry_type.h"
+#include "types/time_stamp_type.h"
+
+#include <glib.h>
+
+struct HashTable {
+    GHashTable *hash_table;
+};
+
+bool
+HashTable__init(struct HashTable *me);
+
+struct LookupReturn
+HashTable__lookup(struct HashTable *me, EntryType key);
+
+enum PutUniqueStatus
+HashTable__put_unique(struct HashTable *me, EntryType key, TimeStampType value);
+
+void
+HashTable__destroy(struct HashTable *me);
