032e36010835074bf15031e271a9bc7982f074bf
David Chu <thedavidchu@gmail.com>
2024-05-05 12:26:20 -0400

    Make trace_test robust against file not found

--- test/lib_test/trace_test/trace_test.c
+++ test/lib_test/trace_test/trace_test.c
@@ -1,11 +1,17 @@
 #include <glib.h>
+#include <stdlib.h>
 
+#include "logger/logger.h"
 #include "trace/reader.h"
 
 int
-main(void)
+main(int argc, char **argv)
 {
-    struct Trace trace = read_trace("/home/david/Downloads/src2.bin");
+    if (argc != 2) {
+        LOGGER_ERROR("expecting trace file name as argument");
+        return EXIT_FAILURE;
+    }
+    struct Trace trace = read_trace(argv[1]);
     g_assert_nonnull(trace.trace);
     return 0;
 }
