8db8abf64f66ccbbb10b72fac7e4c3493ea4d95d
David Chu <thedavidchu@gmail.com>
2024-05-31 12:38:34 -0400

    Add test on single step function

--- test/mrc_test/average_eviction_time_test.c
+++ test/mrc_test/average_eviction_time_test.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "arrays/array_size.h"
 #include "average_eviction_time/average_eviction_time.h"
@@ -8,6 +9,8 @@
 #include "olken/olken.h"
 #include "random/zipfian_random.h"
 #include "test/mytester.h"
+#include "trace/generator.h"
+#include "trace/trace.h"
 
 static bool
 access_same_key_five_times(void)
@@ -37,11 +40,48 @@ access_same_key_five_times(void)
     return true;
 }
 
+static bool
+test_on_step_trace(void)
+{
+    struct Trace trace = generate_step_trace(100, 10);
+    if (trace.trace == NULL || trace.length != 100) {
+        LOGGER_ERROR("bad trace");
+        return false;
+    }
+
+    struct AverageEvictionTime me = {0};
+    struct Olken oracle = {0};
+    g_assert_true(AverageEvictionTime__init(&me, 10, 1));
+    g_assert_true(Olken__init(&oracle, 10, 1));
+
+    for (size_t i = 0; i < trace.length; ++i) {
+        uint64_t key = trace.trace[i].key;
+        g_assert_true(AverageEvictionTime__access_item(&me, key));
+        g_assert_true(Olken__access_item(&oracle, key));
+    }
+
+    g_assert_true(
+        Histogram__debug_difference(&me.histogram, &oracle.histogram, 10));
+
+    // Check the MAE exactly matches
+    struct MissRateCurve mrc = {0}, oracle_mrc = {0};
+    g_assert_true(MissRateCurve__init_from_histogram(&mrc, &me.histogram));
+    g_assert_true(
+        MissRateCurve__init_from_histogram(&oracle_mrc, &oracle.histogram));
+    double const mae = MissRateCurve__mean_absolute_error(&mrc, &oracle_mrc);
+    g_assert_cmpfloat(mae, ==, 0.0);
+
+    AverageEvictionTime__destroy(&me);
+    Olken__destroy(&oracle);
+    return true;
+}
+
 int
 main(int argc, char **argv)
 {
     UNUSED(argc);
     UNUSED(argv);
     ASSERT_FUNCTION_RETURNS_TRUE(access_same_key_five_times());
+    ASSERT_FUNCTION_RETURNS_TRUE(test_on_step_trace());
     return 0;
 }
