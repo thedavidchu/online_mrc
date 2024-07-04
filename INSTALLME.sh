06e1eed40a7472362482682e9a825d9bcec86585
David Chu <45278895+thedavidchu@users.noreply.github.com>
2024-07-04 17:39:15 +0000

    Add INSTALLME.sh (#60)

---
+++ INSTALLME.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+### @brief  This is the install script on a Debian-like machine.
+### @note   This assumes you have a clean repository.
+### @note   You need sudo privileges to run this script. If not, then
+###         you can try building the required files from source and/or
+###         use Python's PIP.
+### @note   You will need access to Professor Ashvin Goel's QuickMRC
+###         repository for this script to work.
+### @todo   Allow building if Professor Ashvin Goel's QuickMRC repository
+###         is not found.
+
+# Make sure we are in the top level of this repository
+cd "$(git rev-parse --show-toplevel)"
+
+# Recursively clone repositories
+# NOTE  You may need to ener your git credentials
+git submodule update --init --recursive
+
+# Install dependencies
+sudo apt update
+sudo apt upgrade
+sudo apt install python3 python3-pip python3-setuptools \
+                       python3-wheel ninja-build meson
+sudo apt install python3-numpy python3-matplotlib python3-tqdm
+sudo apt install pkgconfig libglib2.0-dev
+
+# Build Meson
+meson setup build
+cd build
+meson compile
