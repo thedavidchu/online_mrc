9da60e8af75b75ac4869f48ca9930c167ab3ef06
David Chu <thedavidchu@gmail.com>
2024-07-10 20:54:13 +0000

    Gripe about C

--- src/lib/array/include/array/float64_array.h
+++ src/lib/array/include/array/float64_array.h
@@ -1,3 +1,13 @@
+/** It is times like these that I wish I were using C++ for its Standard
+ *  Template Library (STL). Remind me again, why did I choose to use
+ *  pure C? And not just any C, but the archaic C99.
+ *
+ *  Oh, right. It's because I wanted the experience of writing C code
+ *  and because I would probably have made many rookie mistakes in C++
+ *  that
+ *
+ *  Also, PARDA's codebase was written in C, so I just stuck with it.
+ */
 #pragma once
 
 #include <stdbool.h>
