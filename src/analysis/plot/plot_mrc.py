c9a36339d2d95d4ddbd1b2644c02276e8f7ffda7
David Chu <thedavidchu@gmail.com>
2024-07-12 15:17:14 +0000

    Refactor MRC plotter for standardized MRC binary format
    Cleanup unused code

--- src/analysis/plot/plot_mrc.py
+++ src/analysis/plot/plot_mrc.py
@@ -1,12 +1,8 @@
 import argparse
-import json
 import os
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-INFINITY = float("inf")
 
 
 def timing(f):
@@ -28,83 +24,15 @@ def timing(f):
     return wrap
 
 
-def decode_histogram_json(
-    histogram_json: dict[str, float | int | dict[str, float]]
-) -> dict[float, float]:
-    """
-    Expecting a JSON (string or object) of format:
-    {
-        ".type": "Histogram" | "FractionalHistogram",
-        ".num_bins": %u64,
-        ".bin_size": %u64,
-        ".running_sum": %u64,
-        ".histogram": {"%f": %f},
-        ".false_infinity": %f,
-        ".infinity": %f
-    }
-    """
-    assert histogram_json["type"] in {"Histogram", "FractionalHistogram"}
-
-    histogram = {
-        float(key) * histogram_json[".bin_size"]: float(value)
-        for key, value in histogram_json[".histogram"].items()
-    }
-    histogram.update(
-        (
-            (
-                histogram_json[".num_bins"] * histogram_json[".bin_size"],
-                histogram_json[".false_infinity"],
-            ),
-            (INFINITY, histogram_json[".infinity"]),
-        )
-    )
-    histogram = dict(sorted(list(histogram.items())))
-    return histogram
-
-
-def convert_histogram_to_miss_rate_curve(
-    histogram: dict[float, float]
-) -> dict[float, float]:
-    total = sum(histogram.values())
-    running_inverse_total = total
-    mrc = {}
-    for key, value in histogram.items():
-        mrc[key] = running_inverse_total / total
-        running_inverse_total -= value
-    return mrc
-
-
-def plot_histogram(histogram: dict[float, float]):
-    plt.figure()
-    plt.title("Histogram")
-    plt.xlabel("Reuse Stack Distance")
-    plt.ylabel("Frequency")
-    plt.plot(histogram.keys(), histogram.values())
-    plt.savefig("histogram.png")
-
-
-def plot_miss_rate_curve(path: str, label: str, debug: bool):
-    with open(path) as f:
-        histogram_json = json.load(f)
-    sparse_histogram = decode_histogram_json(histogram_json)
-    sparse_mrc = convert_histogram_to_miss_rate_curve(sparse_histogram)
-    if debug:
-        print(sparse_histogram)
-    plt.plot(sparse_mrc.keys(), sparse_mrc.values(), label=label)
-
-
-def read_and_plot_dense_mrc(path: str, label: str, debug: bool):
-    with open(path, "rb") as f:
-        dense_mrc = np.fromfile(f, dtype=np.float64)
-    if debug:
-        print(dense_mrc)
-    plt.plot(dense_mrc, label=label)
-
-
-def read_and_plot_sparse_mrc(path: str, label: str, debug: bool, separator: str):
+def read_and_plot_mrc(path: str, label: str, debug: bool, separator: str):
     dt = np.dtype([("index", np.uint64), ("miss-rate", np.float64)])
     with open(path, "rb") as f:
         if separator == "":
+            _ = np.fromfile(
+                f,
+                dtype=np.dtype([("num_bins", np.uint64), ("bin_size", np.uint64)]),
+                count=1,
+            )
             sparse_mrc = np.fromfile(f, dtype=dt)
         else:
             sparse_mrc = np.loadtxt(f, dtype=dt, delimiter=separator)
@@ -125,15 +53,13 @@ def plot_from_path(path: str, *, label: str = None, debug: bool = False):
     if label is None:
         label = root
 
-    if ext == ".json":
-        plot_miss_rate_curve(path, label, debug)
-    elif ext == ".bin":
-        read_and_plot_sparse_mrc(path, label, debug, separator="")
+    if ext == ".bin":
+        read_and_plot_mrc(path, label, debug, separator="")
     elif ext == ".dat":
-        read_and_plot_sparse_mrc(path, label, debug, separator=",")
+        read_and_plot_mrc(path, label, debug, separator=",")
     else:
         raise ValueError(
-            f"unrecognized file type for '{path}'. Expecting {{.json,.bin,.dat}}, got {ext}"
+            f"unrecognized file type for '{path}'. Expecting {{.bin,.dat}}, got {ext}"
         )
 
 
