de815cbc9fa2e20e343d270bde0b1ecf5a29a7d7
David Chu <thedavidchu@gmail.com>
2024-07-19 18:30:46 +0000

    [untested] Change Python script to reflect new MRC generator

--- scripts/run_mrc_generator.py
+++ scripts/run_mrc_generator.py
@@ -11,10 +11,7 @@ from pathlib import Path
 from shlex import split
 from subprocess import run, CompletedProcess
 
-ORACLE_EXE = (
-    "/home/david/projects/online_mrc/build/src/analysis/mrc/generate_oracle_exe"
-)
-OUTPUT_EXE = "/home/david/projects/online_mrc/build/src/analysis/mrc/generate_mrc_exe"
+EXE = "/home/david/projects/online_mrc/build/src/analysis/mrc/generate_mrc_exe"
 
 
 def sh(cmd: str, **kwargs) -> CompletedProcess[str]:
@@ -62,33 +59,7 @@ def any_paths_exist(*paths: str) -> bool:
     return any(os.path.exists(p) for p in paths)
 
 
-def run_oracle_on_trace(
-    input_abspath: str, format: str, output_abspath: str, stem: str, overwrite: bool
-):
-    print(f"Started {stem}.bin")
-    trace_abspath = os.path.join(input_abspath, f"{stem}.bin")
-    hist_abspath = os.path.join(output_abspath, f"{stem}-olken-histogram.bin")
-    mrc_abspath = os.path.join(output_abspath, f"{stem}-olken-mrc.bin")
-    log_abspath = os.path.join(output_abspath, f"{stem}-olken.log")
-    if all_paths_exist(hist_abspath, mrc_abspath, log_abspath):
-        if not overwrite:
-            print(f"All outputs for {stem}.bin already exist (skipping)")
-            return
-        else:
-            print(f"All outputs for {stem}.bin already exist (running anyway)")
-    elif any_paths_exist(hist_abspath, mrc_abspath, log_abspath):
-        print(f"Some outputs for {stem}.bin already exist (running anyway)")
-    else:
-        print(f"No outputs for {stem}.bin exist (running)")
-    output = sh(
-        f"nohup {ORACLE_EXE} -i {trace_abspath} -f {format} --histogram {hist_abspath} -m {mrc_abspath}"
-    )
-    with open(log_abspath, "w") as f:
-        f.write(output.stdout)
-    print(f"Finished {stem}.bin")
-
-
-def run_output_on_trace(
+def run_algorithm_on_trace(
     algorithm: str,
     input_abspath: str,
     format: str,
@@ -98,9 +69,9 @@ def run_output_on_trace(
 ):
     print(f"Started {stem}.bin")
     trace_abspath = os.path.join(input_abspath, f"{stem}.bin")
-    hist_abspath = os.path.join(output_abspath, f"{stem}-{algorithm}-histogram.bin")
+    hist_abspath = os.path.join(output_abspath, f"{stem}-{algorithm}-hist.bin")
     mrc_abspath = os.path.join(output_abspath, f"{stem}-{algorithm}-mrc.bin")
-    log_abspath = os.path.join(output_abspath, f"{stem}-{algorithm}.log")
+    log_abspath = os.path.join(output_abspath, f"{stem}.log")
     if all_paths_exist(hist_abspath, mrc_abspath, log_abspath):
         if not overwrite:
             print(f"All {algorithm} outputs for {stem}.bin already exist (skipping)")
@@ -113,12 +84,18 @@ def run_output_on_trace(
         print(f"Some {algorithm} outputs for {stem}.bin already exist (running anyway)")
     else:
         print(f"No {algorithm} outputs for {stem}.bin exist (running)")
-    # NOTE  I set the initial sampling rate to 1e-1 by default without
-    #       a way of modifying this.
-    # NOTE  For a fair comparison, I set the number of histogram bins to
-    #       be the same default number as in the Olken implementation.
+    if algorithm == "Olken":
+        # NOTE  This isn't necessary but I don't want to end the output
+        #       string on a trailing comma.
+        other_data = "mode=realloc"
+    elif algorithm == "Evicting-Map":
+        other_data = "sampling=1e-1,mode=realloc"
+    elif algorithm == "Fixed-Size-SHARDS":
+        other_data = "sampling=1e-1,mode=realloc"
+    else:
+        raise ValueError(f"unrecognized algorithm {algorithm}")
     output = sh(
-        f"nohup {OUTPUT_EXE} -i {trace_abspath} -a {algorithm} -f {format} --histogram {hist_abspath} -o {mrc_abspath} -s 1e-1 --hist-num-bins {1<<20}"
+        f"nohup {EXE} -i {trace_abspath} -f {format} -r {algorithm}(mrc={mrc_abspath},hist={hist_abspath},{other_data})"
     )
     with open(log_abspath, "w") as f:
         f.write(output.stdout)
@@ -172,11 +149,16 @@ def main():
         #       so I decided to just run it all single threaded because
         #       I have all weekend to run it, yay!
         if args.oracle:
-            run_oracle_on_trace(
-                input_abspath, args.format, output_abspath, stem, args.overwrite
+            run_algorithm_on_trace(
+                "Olken",
+                input_abspath,
+                args.format,
+                output_abspath,
+                stem,
+                args.overwrite,
             )
         if args.emap:
-            run_output_on_trace(
+            run_algorithm_on_trace(
                 "Evicting-Map",
                 input_abspath,
                 args.format,
@@ -185,7 +167,7 @@ def main():
                 args.overwrite,
             )
         if args.fs_shards:
-            run_output_on_trace(
+            run_algorithm_on_trace(
                 "Fixed-Size-SHARDS",
                 input_abspath,
                 args.format,
