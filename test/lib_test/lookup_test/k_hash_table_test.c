6d3e0d6e26446d805ccd6fff115f3d56f12eefa9
David Chu <thedavidchu@gmail.com>
2024-08-14 13:36:13 +0000

    [timeout] Make bigger k_hash_table_test

--- test/lib_test/lookup_test/k_hash_table_test.c
+++ test/lib_test/lookup_test/k_hash_table_test.c
@@ -4,74 +4,91 @@
 
 #include <glib.h>
 
-#include "logger/logger.h"
 #include "lookup/k_hash_table.h"
-
 #include "lookup/lookup.h"
 #include "test/mytester.h"
 
-bool
+#define MAX_SIZE (1 << 20)
+#define STREAM   NULL
+
+static bool
 test_khash(void)
 {
     struct KHashTable me = {0};
     g_assert_true(KHashTable__init(&me));
-    g_assert_true(KHashTable__write(&me, stdout, true));
+    KHashTable__write(&me, STREAM, true);
 
-    // Test successful lookups
-    for (uint64_t i = 0; i < 10; ++i) {
+    // Test successful inserts
+    for (uint64_t i = 0; i < MAX_SIZE; ++i) {
         enum PutUniqueStatus r = KHashTable__put_unique(&me, i, 2 * i);
         g_assert_cmpint(r, ==, LOOKUP_PUTUNIQUE_INSERT_KEY_VALUE);
-        KHashTable__write(&me, stdout, true);
+        KHashTable__write(&me, STREAM, true);
     }
 
     // Test successful lookups
-    for (uint64_t i = 0; i < 10; ++i) {
+    for (uint64_t i = 0; i < MAX_SIZE; ++i) {
         struct LookupReturn r = KHashTable__lookup(&me, i);
         g_assert_true(r.success);
         g_assert_cmpuint(r.timestamp, ==, 2 * i);
     }
 
     // Test unsuccessful lookups
-    for (uint64_t i = 10; i < 20; ++i) {
+    for (uint64_t i = MAX_SIZE; i < MAX_SIZE + 10; ++i) {
         struct LookupReturn r = KHashTable__lookup(&me, i);
         g_assert_false(r.success);
     }
 
     // Test successful replacements
-    for (uint64_t i = 0; i < 10; ++i) {
+    for (uint64_t i = 0; i < MAX_SIZE; ++i) {
         enum PutUniqueStatus r = KHashTable__put_unique(&me, i, 3 * i);
         g_assert_cmpint(r, ==, LOOKUP_PUTUNIQUE_REPLACE_VALUE);
-        KHashTable__write(&me, stdout, true);
+        KHashTable__write(&me, STREAM, true);
     }
 
     // Test successful lookups
-    for (uint64_t i = 0; i < 10; ++i) {
+    for (uint64_t i = 0; i < MAX_SIZE; ++i) {
         struct LookupReturn r = KHashTable__lookup(&me, i);
         g_assert_true(r.success);
         g_assert_cmpuint(r.timestamp, ==, 3 * i);
     }
 
     // Test unsuccessful lookups
-    for (uint64_t i = 10; i < 20; ++i) {
+    for (uint64_t i = MAX_SIZE; i < MAX_SIZE + 10; ++i) {
         struct LookupReturn r = KHashTable__lookup(&me, i);
         g_assert_false(r.success);
     }
 
     // Test successful deletes
-    for (uint64_t i = 0; i < 10; ++i) {
+    for (uint64_t i = 0; i < MAX_SIZE; ++i) {
         struct LookupReturn r = KHashTable__remove(&me, i);
         g_assert_true(r.success);
         g_assert_cmpuint(r.timestamp, ==, 3 * i);
-        KHashTable__write(&me, stdout, true);
+        KHashTable__write(&me, STREAM, true);
     }
 
     // Test unsuccessful deletes
-    for (uint64_t i = 10; i < 20; ++i) {
+    for (uint64_t i = MAX_SIZE; i < MAX_SIZE + 10; ++i) {
         struct LookupReturn r = KHashTable__remove(&me, i);
         g_assert_false(r.success);
     }
 
-    KHashTable__write(&me, stdout, true);
+    KHashTable__write(&me, STREAM, true);
+    KHashTable__destroy(&me);
+    return true;
+}
+
+/// @brief  Test the ability to store more than 4 billion elements.
+static bool
+test_large_khash(size_t const size)
+{
+    struct KHashTable me = {0};
+    g_assert_true(KHashTable__init(&me));
+
+    for (uint64_t i = 0; i < size; ++i) {
+        enum PutUniqueStatus r = KHashTable__put_unique(&me, i, 2 * i);
+        g_assert_cmpint(r, ==, LOOKUP_PUTUNIQUE_INSERT_KEY_VALUE);
+    }
+
     KHashTable__destroy(&me);
     return true;
 }
@@ -80,5 +97,6 @@ int
 main(void)
 {
     ASSERT_FUNCTION_RETURNS_TRUE(test_khash());
+    ASSERT_FUNCTION_RETURNS_TRUE(test_large_khash(((size_t)1 << 32) + 1));
     return EXIT_SUCCESS;
 }
