ae246076f0215f23fdbf6ff48510a9e843b7e03f
David Chu <thedavidchu@gmail.com>
2024-08-15 17:27:59 +0000

    Print summary to stdout

--- src/analysis/mrc/deprecated_generate_mrc.c
+++ src/analysis/mrc/deprecated_generate_mrc.c
@@ -245,7 +245,7 @@ static void
 print_trace_summary(struct CommandLineArguments const *args,
                     struct Trace const *const trace)
 {
-    fprintf(stderr,
+    fprintf(stdout,
             "Trace(source='%s', format='%s', length=%zu)\n",
             args->input_path,
             TRACE_FORMAT_STRINGS[args->trace_format],
