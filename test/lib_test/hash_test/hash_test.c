551218cb0aa805c9a41bf9b88075e1430eae703b
David Chu <thedavidchu@gmail.com>
2024-08-22 19:50:37 +0000

    Fix hash_test.c

--- test/lib_test/hash_test/hash_test.c
+++ test/lib_test/hash_test/hash_test.c
@@ -8,6 +8,7 @@
 
 #include "hash/MurmurHash3.h"
 #include "hash/hash.h"
+#include "hash/types.h"
 #include "logger/logger.h"
 #include "test/mytester.h"
 
@@ -36,24 +37,32 @@ test_uint64_hash_to_uint128(void)
     g_assert_cmpuint(hash[0], ==, 2945182322382062539ULL);
     g_assert_cmpuint(hash[1], ==, 17462001654787800658ULL);
 
-    // Test hash() wrapper
-    g_assert_cmpuint(hash[0], ==, Hash64Bit(0));
     return true;
 }
 
+/// @brief  Check my hash wrappers to make sure they are consistent.
+/// @note   I do not make any guarantees about which hash functions I
+///         use. Moreover, I do not guarantee any specific relationship
+///         between hash functions (e.g. I used to have it that the 64
+///         bit hash function would be the first half of the 128 bit).
 static bool
 test_my_murmur_hash_wrappers(void)
 {
-    // NOTE I'm mostly checking to avoid segmentation faults.
-    uint64_t input = 0;
+    uint64_t const zero = 0;
+    g_assert_cmpuint(Hash32Bit(zero), ==, Hash32Bit(zero));
+    g_assert_cmpuint(Hash64Bit(zero), ==, Hash64Bit(zero));
+    g_assert_cmpuint(Hash128Bit(zero).hash[0], ==, Hash128Bit(zero).hash[0]);
+    g_assert_cmpuint(Hash128Bit(zero).hash[1], ==, Hash128Bit(zero).hash[1]);
 
-    g_assert_cmpuint(Hash32Bit(input), ==, 1669671676UL);
-    g_assert_cmpuint(Hash64Bit(input), ==, 2945182322382062539ULL);
-    g_assert_cmpuint(Hash128Bit(input).hash[0], ==, 2945182322382062539ULL);
-    g_assert_cmpuint(Hash128Bit(input).hash[1], ==, 17462001654787800658ULL);
-
-    // NOTE The 64 bit hash is just the first 8 bytes of the 128 bit hash!
-    g_assert_cmpuint(Hash64Bit(input), ==, Hash128Bit(input).hash[0]);
+    // NOTE I am making sure that the hash functions are suitably good.
+    //      This is probabilistically true; I just picked these numbers
+    //      randomly, so I may need to change them in future.
+    uint64_t const one = 1;
+    uint64_t const two = 2;
+    g_assert_cmpuint(Hash32Bit(one), !=, Hash32Bit(two));
+    g_assert_cmpuint(Hash64Bit(one), !=, Hash64Bit(two));
+    g_assert_cmpuint(Hash128Bit(one).hash[0], !=, Hash128Bit(two).hash[0]);
+    g_assert_cmpuint(Hash128Bit(one).hash[1], !=, Hash128Bit(two).hash[1]);
 
     return true;
 }
