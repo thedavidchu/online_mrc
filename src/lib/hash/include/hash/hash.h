01de6cd55da4406baff09ff941060f3d70f48b25
David Chu <thedavidchu@gmail.com>
2024-08-23 18:37:26 +0000

    Add selection for hash function for 64 bits

--- src/lib/hash/include/hash/hash.h
+++ src/lib/hash/include/hash/hash.h
@@ -1,33 +1,49 @@
+/** @brief  Wrappers for hash functions. */
 #pragma once
 
 #include <stdint.h>
 
 #include "hash/MurmurHash3.h"
+#include "hash/miscellaneous_hash.h"
 #include "hash/splitmix64.h"
 #include "hash/types.h"
 #include "types/key_type.h"
 
-/// @brief  Wrapper around MurmurHash3.
+#ifndef HASH_FUNCTION_SELECT
+#define HASH_FUNCTION_SELECT 0
+#endif
+
 static inline Hash32BitType
-Hash32Bit(KeyType key)
+Hash32Bit(KeyType const key)
 {
     uint32_t hash = 0;
     MurmurHash3_x86_32(&key, sizeof(key), 0, &hash);
     return hash;
 }
 
-/// @brief  Wrapper around MurmurHash3.
 static inline Hash64BitType
-Hash64Bit(KeyType key)
+Hash64Bit(KeyType const key)
 {
+#if HASH_FUNCTION_SELECT == 0
     uint64_t hash[2] = {0, 0};
     MurmurHash3_x64_128(&key, sizeof(key), 0, hash);
     return hash[0];
+#elif HASH_FUNCTION_SELECT == 1
+    return splitmix64_hash(key);
+#elif HASH_FUNCTION_SELECT == 2
+    return RSHash((void const *)&key, sizeof(key));
+#elif HASH_FUNCTION_SELECT == 3
+    return SDBMHash((void const *)&key, sizeof(key));
+#elif HASH_FUNCTION_SELECT == 4
+    return APHash((void const *)&key, sizeof(key));
+#else
+#error "invalid value for HASH_FUNCTION_SELECT selected!"
+    return key;
+#endif
 }
 
-/// @brief  Wrapper around MurmurHash3.
 static inline struct Hash128BitType
-Hash128Bit(KeyType key)
+Hash128Bit(KeyType const key)
 {
     struct Hash128BitType hash = {0};
     MurmurHash3_x64_128(&key, sizeof(key), 0, &hash.hash);
