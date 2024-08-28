ad72484539e02383fb86819589d59927f135694a
David Chu <thedavidchu@gmail.com>
2024-08-28 14:41:03 +0000

    Break down hash table performance by function

--- bench/lookup_test/lookup_performance_test.c
+++ bench/lookup_test/lookup_performance_test.c
@@ -13,26 +13,44 @@
 
 size_t const NUM_VALUES_FOR_PERF = 1 << 20;
 
+/// @brief  Test the performance of various domains of the hash table.
+/// @note   I do not test remove operations, because these are not
+///         important to me at the moment. I do not use remove
+///         operations frequently.
 static inline void
-time_lookup(char const *const name,
-            void *const object,
-            enum PutUniqueStatus (*put)(void *const object,
-                                        uint64_t const key,
-                                        uint64_t const value),
-            struct LookupReturn (*lookup)(void const *const object,
-                                          uint64_t const key),
-            void (*destroy)(void *const object))
+time_hash_table(char const *const name,
+                void *const object,
+                enum PutUniqueStatus (*put)(void *const object,
+                                            uint64_t const key,
+                                            uint64_t const value),
+                struct LookupReturn (*lookup)(void const *const object,
+                                              uint64_t const key),
+                void (*destroy)(void *const object))
 {
     double const t0 = get_wall_time_sec();
     for (size_t i = 0; i < NUM_VALUES_FOR_PERF; ++i) {
         put(object, i, i);
     }
+    double const t1 = get_wall_time_sec();
     for (size_t i = 0; i < NUM_VALUES_FOR_PERF; ++i) {
         lookup(object, i);
     }
-    double const t1 = get_wall_time_sec();
+    double const t2 = get_wall_time_sec();
+    for (size_t i = 0; i < NUM_VALUES_FOR_PERF; ++i) {
+        lookup(object, i + NUM_VALUES_FOR_PERF);
+    }
+    double const t3 = get_wall_time_sec();
     destroy(object);
-    LOGGER_INFO("%s time: %f", name, t1 - t0);
+    double const t4 = get_wall_time_sec();
+    LOGGER_INFO("%s -- put time: %f | lookup time: %f | lookup miss time: %f | "
+                "destroy time: %f | "
+                "total time: %f",
+                name,
+                t1 - t0,
+                t2 - t1,
+                t3 - t2,
+                t4 - t3,
+                t4 - t0);
 }
 
 int
@@ -40,36 +58,36 @@ main(void)
 {
     struct BoostHashTable bht = {0};
     BoostHashTable__init(&bht);
-    time_lookup("Boost Hash Table",
-                &bht,
-                (enum PutUniqueStatus(*)(void *const,
-                                         uint64_t const,
-                                         uint64_t const))BoostHashTable__put,
-                (struct LookupReturn(*)(void const *const,
-                                        uint64_t const))BoostHashTable__lookup,
-                (void (*)(void *const))BoostHashTable__destroy);
+    time_hash_table(
+        "Boost Hash Table",
+        &bht,
+        (enum PutUniqueStatus(*)(void *const, uint64_t const, uint64_t const))
+            BoostHashTable__put,
+        (struct LookupReturn(*)(void const *const,
+                                uint64_t const))BoostHashTable__lookup,
+        (void (*)(void *const))BoostHashTable__destroy);
 
     struct HashTable ght = {0};
     HashTable__init(&ght);
-    time_lookup("GLib Hash Table",
-                &ght,
-                (enum PutUniqueStatus(*)(void *const,
-                                         uint64_t const,
-                                         uint64_t const))HashTable__put,
-                (struct LookupReturn(*)(void const *const,
-                                        uint64_t const))HashTable__lookup,
-                (void (*)(void *const))HashTable__destroy);
+    time_hash_table("GLib Hash Table",
+                    &ght,
+                    (enum PutUniqueStatus(*)(void *const,
+                                             uint64_t const,
+                                             uint64_t const))HashTable__put,
+                    (struct LookupReturn(*)(void const *const,
+                                            uint64_t const))HashTable__lookup,
+                    (void (*)(void *const))HashTable__destroy);
 
     struct KHashTable kht = {0};
     KHashTable__init(&kht);
-    time_lookup("KLib Hash Table",
-                &kht,
-                (enum PutUniqueStatus(*)(void *const,
-                                         uint64_t const,
-                                         uint64_t const))KHashTable__put,
-                (struct LookupReturn(*)(void const *const,
-                                        uint64_t const))KHashTable__lookup,
-                (void (*)(void *const))KHashTable__destroy);
+    time_hash_table("KLib Hash Table",
+                    &kht,
+                    (enum PutUniqueStatus(*)(void *const,
+                                             uint64_t const,
+                                             uint64_t const))KHashTable__put,
+                    (struct LookupReturn(*)(void const *const,
+                                            uint64_t const))KHashTable__lookup,
+                    (void (*)(void *const))KHashTable__destroy);
 
     return 0;
 }
