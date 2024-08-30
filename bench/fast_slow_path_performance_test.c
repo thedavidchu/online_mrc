a25c78019cbfbc5d80837602226f265998c9fdf2
David Chu <thedavidchu@gmail.com>
2024-08-30 20:02:52 +0000

    Clarify functions

--- bench/fast_slow_path_performance_test.c
+++ bench/fast_slow_path_performance_test.c
@@ -16,10 +16,16 @@
 #include "run/runner_arguments.h"
 #include "run/trace_runner.h"
 #include "trace/trace.h"
+#include "unused/mark_unused.h"
 
-/// @brief  Generate a trace that will hammer a single item.
-static struct Trace
-generate_fastest_trace(size_t const trace_length)
+/// @note   The keyword 'inline' prevents a compiler warning as per:
+///         https://stackoverflow.com/questions/32432596/warning-always-inline-function-might-not-be-inlinable-wattributes
+#define forceinline __attribute__((always_inline)) inline
+
+/// @brief  Generate a trace from a function.
+static forceinline struct Trace
+generate_trace(size_t const trace_length,
+               uint64_t (*const f)(size_t const i, size_t const trace_length))
 {
     struct Trace trace = {0};
     if (!Trace__init(&trace, trace_length)) {
@@ -27,40 +33,30 @@ generate_fastest_trace(size_t const trace_length)
         return (struct Trace){.trace = NULL, .length = 0};
     }
     for (size_t i = 0; i < trace_length; ++i) {
-        trace.trace[i].key = 0;
+        trace.trace[i].key = f(i, trace_length);
     }
     return trace;
 }
 
-/// @brief  Generate a trace that will run quickly for hash-based samplers.
-static struct Trace
-generate_fast_trace(size_t const trace_length)
+static uint64_t
+hammer_single_element(size_t const i, size_t const trace_length)
 {
-    struct Trace trace = {0};
-    if (!Trace__init(&trace, trace_length)) {
-        LOGGER_ERROR("uninitialized trace");
-        return (struct Trace){.trace = NULL, .length = 0};
-    }
-    for (size_t i = 0; i < trace_length; ++i) {
-        trace.trace[i].key = reverse_splitmix64_hash(i);
-    }
-    return trace;
+    UNUSED(i);
+    UNUSED(trace_length);
+    return 0;
 }
 
-/// @brief  Generate a trace that will run slowly for hash-based samplers.
-static struct Trace
-generate_slow_trace(size_t const trace_length)
+static uint64_t
+increasing_hashes(size_t const i, size_t const trace_length)
 {
-    struct Trace trace = {0};
-    if (!Trace__init(&trace, trace_length)) {
-        LOGGER_ERROR("uninitialized trace");
-        return (struct Trace){.trace = NULL, .length = 0};
-    }
-    for (size_t i = 0; i < trace_length; ++i) {
-        trace.trace[i].key =
-            reverse_splitmix64_hash(REVERSE_INDEX(i, trace_length));
-    }
-    return trace;
+    UNUSED(trace_length);
+    return reverse_splitmix64_hash(i);
+}
+
+static uint64_t
+decreasing_hashes(size_t const i, size_t const trace_length)
+{
+    return reverse_splitmix64_hash(REVERSE_INDEX(i, trace_length));
 }
 
 static bool
@@ -88,7 +84,7 @@ main(void)
                                        "Fixed-Size-SHARDS(sampling=1e-1)",
                                        NULL};
     // Test fastest trace
-    trace = generate_fastest_trace(trace_length);
+    trace = generate_trace(trace_length, hammer_single_element);
     if (!run_trace(runner_args_array, &trace)) {
         LOGGER_ERROR("fast path failed");
         exit(EXIT_FAILURE);
@@ -96,7 +92,7 @@ main(void)
     Trace__destroy(&trace);
 
     // Test fast trace
-    trace = generate_fast_trace(trace_length);
+    trace = generate_trace(trace_length, increasing_hashes);
     if (!run_trace(runner_args_array, &trace)) {
         LOGGER_ERROR("fast path failed");
         exit(EXIT_FAILURE);
@@ -104,7 +100,7 @@ main(void)
     Trace__destroy(&trace);
 
     // Test slow trace
-    trace = generate_slow_trace(trace_length);
+    trace = generate_trace(trace_length, decreasing_hashes);
     if (!run_trace(runner_args_array, &trace)) {
         LOGGER_ERROR("slow path failed");
         exit(EXIT_FAILURE);
