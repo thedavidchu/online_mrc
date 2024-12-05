40c32e0e4ce5a8cd36ec07191fb748bfe55e54b0
David Chu <thedavidchu@gmail.com>
2024-12-05 13:03:15 +0000

    Display many types of working set sizes

--- src/analysis/wss/working_set_size.py
+++ src/analysis/wss/working_set_size.py
@@ -1,3 +1,10 @@
+"""
+Calculate the working set size of a trace.
+
+@note   I use pure Python to calculate the working set sizes, so be
+        careful on large traces!
+"""
+
 import argparse
 from pathlib import Path
 
@@ -42,7 +49,28 @@ def main():
     args = parser.parse_args()
 
     x = np.fromfile(args.input, dtype=TRACE_DTYPES[args.format])
-    print(f"Working set size: {len({i for i in tqdm(x[:]["key"])})}")
+    if args.format == "Kia":
+        x = x[x[:]["command"] == 0]
+
+    # I separate the computationally cheap statistics from the expensive ones.
+    print(f"Number accesses: {len(x)}")
+    print(f"Number unique objects: {len(np.unique(x[:]["key"]))}")
+    print(f"Total bytes requested: {np.sum(x[:]["size"])}")
+
+    # Get working set sizes (may take a while!).
+    max_cache_size = {}
+    first_cache_size = {}
+    last_cache_size = {}
+    for obj in tqdm(x):
+        key = obj["key"]
+        size = obj["size"]
+        max_cache_size[key] = max(max_cache_size.get(key, 0), size)
+        last_cache_size[key] = size
+        if key not in first_cache_size:
+            first_cache_size[key] = size
+    print(f"Working set size [bytes]: {sum(max_cache_size.values())}")
+    print(f"Naive WSS (based on first size) [bytes]: {sum(first_cache_size.values())}")
+    print(f"Naive WSS (based on last size) [bytes]: {sum(last_cache_size.values())}")
 
 
 if __name__ == "__main__":
