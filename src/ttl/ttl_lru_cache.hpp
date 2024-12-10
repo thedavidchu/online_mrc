08231d5da642b0779e866784f6311e55a00d3b6f
David Chu <thedavidchu@gmail.com>
2024-12-10 14:54:10 +0000

    Fix TTL-LRU implementation

--- src/ttl/ttl_lru_cache.hpp
+++ src/ttl/ttl_lru_cache.hpp
@@ -9,7 +9,6 @@
 #include "math/saturation_arithmetic.h"
 
 struct myTTLForLRU {
-    uint64_t insert_time_ms;
     uint64_t last_access_time_ms;
     uint64_t ttl_s;
 };
@@ -31,32 +30,43 @@ public:
     {
     }
 
+    static std::uint64_t
+    get_expiry_time_ms(std::uint64_t const access_time_ms,
+                       std::uint64_t const ttl_s)
+    {
+        return saturation_add(access_time_ms, saturation_multiply(1000, ttl_s));
+    }
+
+    int
+    evict_soonest_expiring()
+    {
+        auto const x = expiration_queue_.begin();
+        std::uint64_t victim_key = x->second;
+        expiration_queue_.erase(x);
+        std::size_t i = map_.erase(victim_key);
+        assert(i == 1);
+        assert(map_.size() + 1 == capacity_);
+        return 0;
+    }
+
     int
     access_item(std::uint64_t const key)
     {
         assert(map_.size() == expiration_queue_.size());
-        if (map_.size() >= capacity_) {
-            auto const x = expiration_queue_.begin();
-            std::uint64_t victim_key = x->second;
-            expiration_queue_.erase(x);
-            std::size_t i = map_.erase(victim_key);
-            assert(i == 1);
-            assert(map_.size() + 1 == capacity_);
-        }
 
         if (map_.count(key)) {
             struct myTTLForLRU &s = map_[key];
 
             // Use old eviction time
-            std::uint64_t old_evict_time = s.last_access_time_ms + s.ttl_s;
+            std::uint64_t old_evict_time =
+                TTLLRUCache::get_expiry_time_ms(s.last_access_time_ms, s.ttl_s);
             auto const [start, end] =
                 expiration_queue_.equal_range(old_evict_time);
             for (auto i = start; i != end; ++i) {
                 if (i->second == key) {
                     auto node_handle = expiration_queue_.extract(i);
                     node_handle.key() =
-                        saturation_add(logical_time_,
-                                       saturation_multiply(1000, ttl_s_));
+                        TTLLRUCache::get_expiry_time_ms(logical_time_, ttl_s_);
                     expiration_queue_.insert(std::move(node_handle));
                     break;
                 }
@@ -68,14 +78,15 @@ public:
 
             statistics_.hit();
         } else {
+            if (map_.size() >= capacity_) {
+                this->evict_soonest_expiring();
+            }
             map_[key] = {
-                logical_time_,
                 logical_time_,
                 ttl_s_,
             };
             uint64_t eviction_time_ms =
-                saturation_add(logical_time_,
-                               saturation_multiply(1000, ttl_s_));
+                TTLLRUCache::get_expiry_time_ms(logical_time_, ttl_s_);
             expiration_queue_.emplace(eviction_time_ms, key);
 
             statistics_.miss();
