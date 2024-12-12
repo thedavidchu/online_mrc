89021e9e6239cb988f9ba73d02bb116db2c01251
David Chu <thedavidchu@gmail.com>
2024-12-12 19:00:46 +0000

    Add option to count printed in trace printer

--- src/analysis/text/print_trace.c
+++ src/analysis/text/print_trace.c
@@ -33,6 +33,7 @@ struct CommandLineArguments {
     gchar *input_path;
     enum TraceFormat trace_format;
     gint command;
+    gboolean count_printed_only;
     gint64 start;
     gint64 length;
 };
@@ -92,6 +93,7 @@ parse_command_line_arguments(int argc, char *argv[])
                                         .input_path = NULL,
                                         .trace_format = TRACE_FORMAT_KIA,
                                         .command = -1,
+                                        .count_printed_only = FALSE,
                                         .start = 0,
                                         .length = 10};
     gchar *trace_format = NULL;
@@ -127,6 +129,15 @@ parse_command_line_arguments(int argc, char *argv[])
          &args.command,
          "filter for a specific command. Default: -1 (i.e. print any "
          "commands)",
+         NULL},
+        {"printed",
+         'p',
+         0,
+         G_OPTION_ARG_NONE,
+         &args.count_printed_only,
+         "only count the number of entries printed, rather than the number of "
+         "entries passed over. Default: false. N.B. Obviously, we won't exceed "
+         "the file size.",
          NULL},
         {"length",
          'l',
@@ -181,32 +192,131 @@ cleanup:
     exit(-1);
 }
 
-static void
-print_trace_entry(struct FullTraceItem const item)
+static bool
+print_header(FILE *const stream, size_t const num_entries)
 {
+    if (stream == NULL) {
+        return false;
+    }
+    fprintf(stdout, "total trace length [entries]: %zu\n", num_entries);
     fprintf(stdout,
-            "%20" PRIu64 " | %7" PRIu8 " | %20" PRIu64 " | %10" PRIu32
-            " | %10" PRIu32 "\n",
-            item.timestamp_ms,
-            item.command,
-            item.key,
-            item.size,
-            item.ttl_s);
+            "%10s | %20s | %1s | %20s | %10s | %10s\n",
+            "ID",
+            "Timestamp [ms]",
+            "Command",
+            "Key",
+            "Size [B]",
+            "TTL [s]");
+    fprintf(stdout,
+            "-----------|----------------------|---------|---------------------"
+            "-|-----------"
+            "-|-----------\n");
+    return true;
+}
+
+static bool
+maybe_print_entry(size_t const id,
+                  void const *const buffer,
+                  int const command,
+                  enum TraceFormat const trace_format)
+{
+    size_t const bytes_per_trace_item = get_bytes_per_trace_item(trace_format);
+    struct FullTraceItemResult r = construct_full_trace_item(
+        &((uint8_t *)buffer)[id * bytes_per_trace_item],
+        trace_format);
+    if (!r.valid) {
+        return false;
+    }
+    struct FullTraceItem item = r.item;
+    if (command == -1 || command == r.item.command) {
+        fprintf(stdout,
+                "%10zu | %20" PRIu64 " | %7" PRIu8 " | %20" PRIu64
+                " | %10" PRIu32 " | %10" PRIu32 "\n",
+                id,
+                item.timestamp_ms,
+                item.command,
+                item.key,
+                item.size,
+                item.ttl_s);
+        return true;
+    }
+    return false;
+}
+
+static bool
+print_entries(struct CommandLineArguments const *const args,
+              struct MemoryMap const *const mm)
+{
+    if (args == NULL || mm == NULL || mm->buffer == NULL) {
+        return false;
+    }
+    size_t const start = args->start;
+    size_t const length = args->length;
+    // This is the earliest the ending could possibly be according to
+    // the user's inputs (N.B. it may still be pasted the end).
+    // That is, if all of the entries are printed, we cannot end before
+    // this point (unless we run out of entries in the trace).
+    size_t const min_requested_end = start + length;
+    size_t const bytes_per_trace_item =
+        get_bytes_per_trace_item(args->trace_format);
+    if (bytes_per_trace_item == 0) {
+        return false;
+    }
+    size_t const num_entries = mm->num_bytes / bytes_per_trace_item;
+    if (!print_header(stdout, num_entries)) {
+        LOGGER_ERROR("failed to print header");
+        return false;
+    }
+    if (start >= num_entries) {
+        LOGGER_ERROR("invalid start (%zu) given only %zu entries",
+                     start,
+                     num_entries);
+        return false;
+    } else if (min_requested_end > num_entries) {
+        LOGGER_WARN("start + length (%zu + %zu) exceeds the "
+                    "number of entries (%zu)",
+                    start,
+                    length,
+                    num_entries);
+    }
+    if (!args->count_printed_only) {
+        size_t const end = MIN(min_requested_end, num_entries);
+        for (size_t i = start; i < end; ++i) {
+            maybe_print_entry(i, mm->buffer, args->command, args->trace_format);
+        }
+    } else {
+        size_t count = 0;
+        size_t i = start;
+        while (count < length && i < num_entries) {
+            bool r = maybe_print_entry(i,
+                                       mm->buffer,
+                                       args->command,
+                                       args->trace_format);
+            if (r) {
+                ++count;
+            }
+            ++i;
+        }
+        // We got to the end of the file before reaching our desired
+        //  number of entries to print.
+        if (i == num_entries && count < length) {
+            LOGGER_WARN("we only printed %zu of the requested %zu entries",
+                        count,
+                        length);
+            return false;
+        }
+    }
+    return true;
 }
 
 static bool
 run(struct CommandLineArguments const *const args)
 {
     struct MemoryMap mm = {0};
-    size_t bytes_per_trace_item = 0;
-    size_t num_entries = 0;
-    size_t start = 0, length = 0, end = 0;
-
     if (args == NULL) {
         LOGGER_ERROR("args is NULL");
         goto cleanup_error;
     }
-
     LOGGER_INFO("CommandLineArguments(executable='%s', input='%s', "
                 "trace_format='%s', start=%" PRId64 ", length=%" PRId64 ")",
                 args->executable,
@@ -214,51 +324,15 @@ run(struct CommandLineArguments const *const args)
                 TRACE_FORMAT_STRINGS[args->trace_format],
                 args->start,
                 args->length);
-    bytes_per_trace_item = get_bytes_per_trace_item(args->trace_format);
-    if (bytes_per_trace_item == 0) {
-        LOGGER_ERROR("invalid input", args->trace_format);
-        goto cleanup_error;
-    }
-
     // Memory map the input trace file
     if (!MemoryMap__init(&mm, args->input_path, "rb")) {
         LOGGER_ERROR("failed to mmap '%s'", args->input_path);
         goto cleanup_error;
     }
-    num_entries = mm.num_bytes / bytes_per_trace_item;
-
-    start = args->start;
-    length = args->length;
-    end = args->start + args->length;
-    if (start >= num_entries || end > num_entries) {
-        LOGGER_ERROR(
-            "invalid start (%zu) or length (%zu) for number of entries (%zu)",
-            start,
-            length,
-            num_entries);
+    if (!print_entries(args, &mm)) {
+        LOGGER_ERROR("failed to print entries");
         goto cleanup_error;
     }
-    LOGGER_INFO("length [entries]: %zu", num_entries);
-    fprintf(stdout,
-            "%20s | %1s | %20s | %10s | %10s\n",
-            "Timestamp [ms]",
-            "Command",
-            "Key",
-            "Size [B]",
-            "TTL [s]");
-    fprintf(stdout,
-            "---------------------|---------|----------------------|-----------"
-            "-|-----------\n");
-    for (size_t i = start; i < end; ++i) {
-        struct FullTraceItemResult r = construct_full_trace_item(
-            &((uint8_t *)mm.buffer)[i * bytes_per_trace_item],
-            args->trace_format);
-        assert(r.valid);
-        if (args->command == -1 || args->command == r.item.command) {
-            print_trace_entry(r.item);
-        }
-    }
-
     MemoryMap__destroy(&mm);
     return true;
 cleanup_error:
