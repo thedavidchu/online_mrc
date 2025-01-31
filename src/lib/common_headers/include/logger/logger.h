f8c7756e5410d96bfae4f8b1608c57a71214342d
David Chu <thedavidchu@gmail.com>
2025-01-31 18:38:54 +0000

    Add timing logger level

--- src/lib/common_headers/include/logger/logger.h
+++ src/lib/common_headers/include/logger/logger.h
@@ -26,10 +26,11 @@ enum LoggerLevels {
     LOGGER_LEVEL_WARN = 4,
     LOGGER_LEVEL_ERROR = 5,
     LOGGER_LEVEL_FATAL = 6,
+    LOGGER_LEVEL_TIMING = 7,
 };
 
 static char const *const LOGGER_LEVEL_STRINGS[] =
-    {"VERBOSE", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
+    {"VERBOSE", "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "TIMING"};
 
 static inline bool
 _logger_header(FILE *const stream,
@@ -146,6 +147,15 @@ _logger(FILE *const stream,
     _logger(LOGGER_STREAM,                                                     \
             LOGGER_LEVEL,                                                      \
             LOGGER_LEVEL_FATAL,                                                \
+            errno,                                                             \
+            __FILE__,                                                          \
+            __LINE__,                                                          \
+            __VA_ARGS__)
+
+#define LOGGER_TIMING(...)                                                     \
+    _logger(LOGGER_STREAM,                                                     \
+            LOGGER_LEVEL,                                                      \
+            LOGGER_LEVEL_TIMING,                                               \
             errno,                                                             \
             __FILE__,                                                          \
             __LINE__,                                                          \
