077fcdae2232506014d7120a4ffd6a7720a1ed61
David Chu <thedavidchu@gmail.com>
2025-03-27 22:07:29 +0000

    Add stringify for eviction cause

--- src/predictor/include/lib/eviction_cause.hpp
+++ src/predictor/include/lib/eviction_cause.hpp
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <string>
+
 enum class EvictionCause {
     LRU,
     TTL,
     VolatileTTL,
     AccessExpired,
 };
+
+static inline std::string
+EvictionCause__string(EvictionCause const cause)
+{
+    switch (cause) {
+    case EvictionCause::LRU:
+        return "LRU";
+    case EvictionCause::TTL:
+        return "TTL";
+    case EvictionCause::VolatileTTL:
+        return "VolatileTTL";
+    case EvictionCause::AccessExpired:
+        return "AccessExpired";
+    default:
+        assert(0 && "impossible");
+    }
+}
