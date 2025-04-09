17651a87bfc06be905b5f458c1bf22bb634ed377
David Chu <thedavidchu@gmail.com>
2025-04-09 19:01:04 +0000

    Make CacheAccessTrace use more const members

--- src/lib/cpp_cache/cache_trace.cpp
+++ src/lib/cpp_cache/cache_trace.cpp
@@ -4,30 +4,39 @@
 #include "cpp_cache/cache_trace_format.hpp"
 #include "io/io.h"
 #include "logger/logger.h"
+#include "math/is_nth_iter.h"
 
 #include <cassert>
 #include <cstddef>
 #include <string>
+#include <sys/mman.h>
+
+static inline struct MemoryMap
+mm_init(char const *const fname)
+{
+    struct MemoryMap mm = {};
+    if (!MemoryMap__init(&mm, fname, "rb")) {
+        LOGGER_ERROR("failed to mmap '%s'", fname);
+        exit(1);
+    }
+    return mm;
+}
 
 CacheAccessTrace::CacheAccessTrace(std::string const &fname,
                                    CacheTraceFormat const format)
     : bytes_per_obj_(CacheTraceFormat__bytes_per_entry(format)),
-      format_(format)
+      format_(format),
+      mm_(mm_init(fname.c_str())),
+      length_(mm_.num_bytes / bytes_per_obj_)
 {
     if (bytes_per_obj_ == 0) {
         LOGGER_ERROR("invalid format %s",
                      CacheTraceFormat__string(format).c_str());
         exit(1);
     }
-    // Memory map the input trace file
-    if (!MemoryMap__init(&mm_, fname.c_str(), "rb")) {
-        LOGGER_ERROR("failed to mmap '%s'", fname.c_str());
-        exit(1);
-    }
-    length_ = mm_.num_bytes / bytes_per_obj_;
 }
 
-CacheAccessTrace::~CacheAccessTrace() { MemoryMap__destroy(&mm_); }
+CacheAccessTrace::~CacheAccessTrace() { MemoryMap__kdestroy(&mm_); }
 
 size_t
 CacheAccessTrace::size() const
