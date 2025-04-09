dbbbf9b3eef481047ee9535e80d870be18c75fa8
David Chu <thedavidchu@gmail.com>
2025-04-09 18:59:40 +0000

    Add is_nth_iter

---
+++ src/lib/common_headers/include/math/is_nth_iter.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* !__cplusplus */
+
+#include <stdbool.h>
+#include <stddef.h>
+
+/// @brief  Calculate if we are on the N-th iteration.
+static inline bool
+is_nth_iter(size_t const i, size_t const n)
+{
+    return (i + 1) % n == 0;
+}
+
+#ifdef __cplusplus
+}
+#endif /* !__cplusplus */
