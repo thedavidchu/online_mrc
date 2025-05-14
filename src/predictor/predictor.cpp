f2762e6b3a0cd3711f4dff005dd994e811416b37
David Chu <thedavidchu@gmail.com>
2025-05-14 13:32:18 +0000

    Add remaining lifetime tracker to main JSON

--- src/predictor/predictor.cpp
+++ src/predictor/predictor.cpp
@@ -89,8 +89,14 @@ run_single_cache(std::promise<std::string> ret,
                   shards_ratio);
 
     FixedRateShardsSampler__json(shards_ss, frss, false);
-    p.print_statistics(ss, {{"SHARDS", shards_ss.str()}});
-    ss << p.record_remaining_lifetime(trace.back()) << std::endl;
+    p.print_statistics(ss,
+                       {
+                           {"SHARDS", shards_ss.str()},
+                           {
+                               "remaining_lifetime",
+                               p.record_remaining_lifetime(trace.back()),
+                           },
+                       });
     ret.set_value(ss.str());
     FixedRateShardsSampler__destroy(&frss);
 
