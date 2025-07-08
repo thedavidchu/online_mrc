51498aa33cd534a675a6ae3cb36c02368b8b10a8
David Chu <thedavidchu@gmail.com>
2025-07-08 01:24:37 +0000

    Refactor accurate plotter to use functions from another file

--- scripts/plot_accurate_cache.py
+++ scripts/plot_accurate_cache.py
@@ -5,71 +5,8 @@ from pathlib import Path
 from typing import Callable
 
 import matplotlib.pyplot as plt
-
-
-def parse_data(input_file: Path) -> list[dict]:
-    """@return [JSONs]"""
-    if not input_file.exists():
-        raise FileNotFoundError(f"'{str(input_file)}' not found")
-    file_data = input_file.read_text().splitlines()
-    # Parse into JSON for each run of the cache
-    accum = []
-    for line in file_data:
-        if line.startswith("> "):
-            try:
-                data = json.loads(line[2:])
-            except json.decoder.JSONDecodeError as e:
-                print(f"bad JSON: '{line[2:]}'")
-                raise e
-
-            accum.append(data)
-    return accum
-
-
-def parse_number(num: str | float | int) -> float | int:
-    """Parse a quantity to canonical units (bytes, milliseconds)."""
-    memsizes = {
-        "TB": 1e12,
-        "GB": 1e9,
-        "MB": 1e6,
-        "KB": 1e3,
-        "TiB": 1 << 40,
-        "GiB": 1 << 30,
-        "MiB": 1 << 20,
-        "KiB": 1 << 10,
-        "B": 1,
-    }
-    plural_times = {
-        "years": 365 * 24 * 3600 * 1000,
-        "days": 24 * 3600 * 1000,
-        "hours": 3600 * 1000,
-        "minutes": 60 * 1000,
-        "seconds": 1000,
-        "milliseconds": 1,
-    }
-    single_times = {
-        "year": 365 * 24 * 3600 * 1000,
-        "day": 24 * 3600 * 1000,
-        "hour": 3600 * 1000,
-        "minute": 60 * 1000,
-        "second": 1000,
-        "millisecond": 1,
-    }
-    short_times = {
-        "h": 3600 * 1000,
-        "min": 60 * 1000,
-        "sec": 1000,
-        "s": 1000,
-        "ms": 1,
-    }
-    if isinstance(num, (float, int)):
-        return num
-    if not num[-1].isalpha():
-        return float(num)
-    val, unit = num.split()
-    return (
-        float(val) * {**memsizes, **plural_times, **single_times, **short_times}[unit]
-    )
+import numpy as np
+from plot_predictive_cache import parse_data, parse_number
 
 
 def data_vs_capacity(
@@ -88,35 +25,15 @@ def data_vs_capacity(
     )
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", type=Path, required=True, help="input file")
-    parser.add_argument("--mrc", type=Path, help="MRC output path")
-    parser.add_argument(
-        "--lifetime-threshold", type=Path, help="Lifetime threshold output path"
+def plot_lifetime_threshold(violin: bool, data_list, output: Path):
+    lifetime_thresholds = data_vs_capacity(
+        data_list,
+        lambda d: {
+            parse_number(f): hist["Histogram"]["total"]
+            for f, hist in d["Oracle"]["Lifetime Thresholds"].items()
+        },
     )
-    args = parser.parse_args()
-
-    input_file = args.input.resolve()
-    data_list = parse_data(input_file)
-
-    if args.mrc is not None:
-        mrc = data_vs_capacity(data_list, lambda d: d["Miss Ratio"])
-        fig, ax = plt.subplots()
-        ax.set_title("Miss Ratio Curve")
-        ax.set_xlabel("Cache Size [GiB]")
-        ax.set_ylabel("Miss Ratio")
-        ax.plot(mrc.keys(), mrc.values(), "x-")
-        ax.set_ylim(0.0, 1.0)
-        fig.savefig(args.mrc.resolve())
-    if args.lifetime_threshold is not None:
-        lifetime_thresholds = data_vs_capacity(
-            data_list,
-            lambda d: {
-                parse_number(f): hist["Histogram"]["total"]
-                for f, hist in d["Lifetime Thresholds"].items()
-            },
-        )
+    if not violin:
         fig, axes = plt.subplots(
             nrows=1,
             ncols=len(lifetime_thresholds),
@@ -135,7 +52,56 @@ def main():
             # data to a Gaussian distribution, which my data isn't.
             ax.plot(hist.values(), hist.keys(), "x-")
             ax.axhline(y=0, color="black", linestyle="--", label="y=0")
-        fig.savefig(args.lifetime_threshold.resolve())
+    else:
+        fig, ax = plt.subplots()
+        fig.suptitle("Distribution of LFU Frequency Evictions")
+        fig.supxlabel("Cache Size [GiB]")
+        fig.supylabel("Distribution of LFU Frequency Evictions")
+        ax.axhline(y=0, color="black", linestyle="--", label="y=0")
+        # I was going to use a violin plot, but it tries to map your
+        # data to a Gaussian distribution, which my data isn't.
+        vpstats = [
+            dict(
+                coords=list(hist.keys()),
+                vals=np.log10(np.array(list(hist.values()))),
+                mean=0,
+                median=0,
+                min=0,
+                max=0,
+            )
+            for hist in lifetime_thresholds.values()
+        ]
+        positions = list(lifetime_thresholds.keys())
+        ax.violin(
+            vpstats,
+            positions,
+        )
+    fig.savefig(output.resolve())
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=Path, required=True, help="input file")
+    parser.add_argument("--mrc", type=Path, help="MRC output path")
+    parser.add_argument(
+        "--lifetime-threshold", type=Path, help="Lifetime threshold output path"
+    )
+    args = parser.parse_args()
+
+    input_file = args.input.resolve()
+    data_list = parse_data(input_file)[(0.0, 1.0, "EvictionTime")]
+
+    if args.mrc is not None:
+        mrc = data_vs_capacity(data_list, lambda d: d["Oracle"]["Miss Ratio"])
+        fig, ax = plt.subplots()
+        ax.set_title("Miss Ratio Curve")
+        ax.set_xlabel("Cache Size [GiB]")
+        ax.set_ylabel("Miss Ratio")
+        ax.plot(mrc.keys(), mrc.values(), "x-")
+        ax.set_ylim(0.0, 1.0)
+        fig.savefig(args.mrc.resolve())
+    if args.lifetime_threshold is not None:
+        plot_lifetime_threshold(True, data_list, args.lifetime_threshold)
 
 
 if __name__ == "__main__":
