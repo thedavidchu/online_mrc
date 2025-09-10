61341f9509f332956641b595ea3bbb57b08201a1
David Chu <thedavidchu@gmail.com>
2025-09-10 17:25:52 +0000

    Add cluster #25 to accurate_run.sh

--- accurate_run.sh
+++ accurate_run.sh
@@ -10,13 +10,13 @@ odir="./myresults/accurate/"
 mkdir -p "$odir"
 
 for policy in TTL CacheLib Memcached Redis; do
-    # for c in 50 15 45 31 12 22 7 19 37 11 24 44 29 6 17 25 52 18; do
-    for c in 12 15 31 37; do
-        if [[ $c -eq 12 || $c -eq 15 || $c -eq 31 || $c -eq 37 ]]; then
-            shards=0.1
-        else
-            shards=0.001
-        fi
+    for c in 50 15 45 31 12 22 7 19 37 11 24 44 29 6 17 25 52 18 25; do
+    # for c in 12 15 31 37; do
+        # if [[ $c -eq 12 || $c -eq 15 || $c -eq 31 || $c -eq 37 ]]; then
+        #     shards=0.1
+        # else
+        #     shards=0.001
+        # fi
         time \
             ./build/src/predictor/accurate_exe \
             /mnt/disk1-20tb/Sari-Traces/FilteredTwitter_Binary/cluster$c.bin \
